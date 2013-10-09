c38034805c2e65f879acf07e47978a03b88cb571
Edward Yang <edward.yang@magnet.com>
2013-10-08 23:49:34 -0700

    production setup - minify client side code, store sessions to redis, log everything to rotating logs, log errors to console and send email
    Former-commit-id: 152a4fad9fe23b83ee40cd2d556aeb79f9721f70
    Former-commit-id: de7ae358f33524c365c750019053df05c90698cf
    Former-commit-id: 8526b6e6d574772cebdf27f3943feb47c48ee02c
    Former-commit-id: c7dae67aad10e8bf2e362d8e1a71d6150e026105

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express')
 , http = require('http')
 , app = express()
-, server = http.createServer(app)
 , connect = require('express/node_modules/connect')
 , fs = require('fs')
 , winston = require('winston');
@@ -14,10 +13,8 @@ require('./lib/orm').setup('./lib/models');
 app.set('port', ENV_CONFIG.App.port);
 
 app.configure(function(){
-
-    /* view rendering */
+    // view rendering
     app.engine('ejs', require('ejs-locals'));
-
     app.set('views', __dirname + '/views');
     app.locals({
         _layoutFile : '/layouts/site'
@@ -25,12 +22,11 @@ app.configure(function(){
     app.locals.open = '{{';
     app.locals.close = '}}';
     app.set('view engine', 'ejs');
-
-    /* app configuration */
+    // allow req.body
     app.use(express.bodyParser());
     app.use(express.cookieParser(ENV_CONFIG.App.sessionSecret));
+    // enable PUT and DELETE request methods
     app.use(express.methodOverride());
-
 });
 
 app.configure('development', function(){
@@ -62,41 +58,66 @@ app.configure('test', function(){
 app.configure('production', function(){
     app.use(express.errorHandler());
     /// TODO: Take out before hitting production
-    // Authentication module.
+    /* Authentication module to prevent authorized access to factory
     var auth = require('http-auth');
     var basic = auth.basic({
         realm : "Authenticated Area.",
         file  : "./data/users.htpasswd" // manager1@magnetapi.com/test
     });
-    //app.use(auth.connect(basic));
-    // log errors to console, log everything to file
-    global.winston = new (winston.Logger)({
-        transports : [
-            new (winston.transports.Console)({
-                level : 'error'
-            }),
-            new (winston.transports.File)({
-                filename : 'target/events/server.log'
-            })
-        ]
+    app.use(auth.connect(basic));
+    */
+    // stop exit after an uncaughtException
+    winston.exitOnError = false;
+    // log only errors to console
+    winston.remove(winston.transports.Console);
+    winston.add(winston.transports.Console, {
+        level            : 'error',
+        handleExceptions : true
     });
+    // log only errors to support email
+    require('winston-mail').Mail;
+    winston.add(winston.transports.Mail, {
+        to               : ENV_CONFIG.Email.supportEmail,
+        from             : ENV_CONFIG.Email.sender,
+        host             : ENV_CONFIG.Email.host,
+        port             : ENV_CONFIG.Email.port,
+        username         : ENV_CONFIG.Email.user,
+        password         : ENV_CONFIG.Email.password,
+        level            : 'error',
+        tls              : true,
+        handleExceptions : true
+    });
+    // log everything to file
+    winston.add(winston.transports.File, {
+        filename         : ENV_CONFIG.Logging.filename,
+        maxsize          : ENV_CONFIG.Logging.maxsize,
+        maxFiles         : ENV_CONFIG.Logging.maxFiles,
+        handleExceptions : true
+    });
+    // store sessions to redis
     var RedisStore = require('connect-redis')(express);
-    connect().use(connect.session({
-        store  :  new RedisStore(),
-        secret : ENV_CONFIG.App.sessionSecret
+    var redisDB = require('redis').createClient(ENV_CONFIG.Redis);
+    app.use(express.session({
+        secret : ENV_CONFIG.App.sessionSecret,
+        store  : new RedisStore({
+            client : redisDB,
+            secret : ENV_CONFIG.App.sessionSecret
+        })
     }));
     // minify client side code and set router to build path
     require('requirejs').optimize(require('./lib/config/ClientBuild'), function(){
         winston.info('Requirejs: successfully optimized client javascript');
     });
-    // prioritize router before public directory
+    // prioritize router before public directory, use minified public directory
     app.use(express.static(__dirname + '/public-build'));
 });
 
 // Routes
 require('./routes')(app);
 
-// Listener
+// Run Webserver
+
+var server = http.createServer(app);
 server.listen(app.get('port'), function(){
     winston.info("Express: server listening on port %d in %s mode", app.get('port'), app.settings.env);
 });
