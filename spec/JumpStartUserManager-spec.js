1f8f5d57f91ed1d1c74b45190e944e4f4ace11b8
Pritesh Shah <pritesh.shah@magnet.com>
2013-10-24 16:14:30 -0700

    Changed JumpStartUserManager to be a singleton so that we dont recreate the connection pool over and over.
    Former-commit-id: 2c03a9b64d060ed57841444b2cdb63106d1b672e
    Former-commit-id: 83fd69c1ed75c84f12db66368ea952d09df858d7
    Former-commit-id: 539ca46911124ce36768af6760cc1d358b3a6dfa
    Former-commit-id: 2a7b3fd40bbe9e8da9bfce560a25ab7817a20c4b

--- spec/JumpStartUserManager-spec.js
+++ spec/JumpStartUserManager-spec.js
@@ -1,18 +1,38 @@
-var JumpStartUserManager = require("../lib/JumpStartUserManager")
-, Helper = require('./Helper')
+var Helper = require('./Helper')
 , sinon = require('sinon')
 , mysql = require('mysql');
 
 jasmine.getEnv().defaultTimeoutInterval = 30000;
 
 describe("JumpStartUserManager", function() {
+    it("should have no pool if isEnabled is false", function() {
+
+        var jsSettings = ENV_CONFIG.JumpStart;
+        delete ENV_CONFIG.JumpStart;
+//        console.log(require.cache);
+        delete require.cache[require.resolve("../lib/JumpStartUserManager.js")];
+        var JumpStartUserManagerWithMissingConfig = require("../lib/JumpStartUserManager");
+        expect(JumpStartUserManagerWithMissingConfig.pool).toBeUndefined();
+
+        ENV_CONFIG.JumpStart = jsSettings;
+    });
+});
+
+describe("JumpStartUserManager", function() {
+
+    delete require.cache[require.resolve("../lib/JumpStartUserManager.js")];
+    var JumpStartUserManager = require("../lib/JumpStartUserManager");
+
     var pool;
     var poolMock;
     var connection;
     var connectionMock;
-    var jumpstartUserManager;
+    var userName;
+    var password;
 
     beforeEach(function() {
+        userName = 'john.appleseed@magnetapi.com';
+        password = 'secure435';
         pool  = mysql.createPool({
             host     : ENV_CONFIG.JumpStart.Database.params.host,
             user     : ENV_CONFIG.JumpStart.Database.username,
@@ -27,20 +47,30 @@ describe("JumpStartUserManager", function() {
 //            done();
 //        });
         // Workaround for the above
-        connection = { query: function() {}, release: function() {} };
+        connection = { query: function() {}, release: function() {}, escape: function() {} };
         connectionMock = sinon.mock(connection);
 
         poolMock.expects("getConnection").once().callsArgWith(0, null, connectionMock.object);
         connectionMock.expects("release").once();
 
-        jumpstartUserManager = new JumpStartUserManager(poolMock.object);
+        expect(JumpStartUserManager.pool).not.toBeNull();
+        JumpStartUserManager.pool = poolMock.object;
+        expect(JumpStartUserManager.isEnabled).toBeTruthy();
     });
 
     describe("createUser", function() {
         it("should succeed if the underlying stored procedure succeeds", function() {
-            connectionMock.expects("query").once().withArgs("CALL user_create('pritesh.shah@magnet.com','Magnet435')").callsArgWith(1, null, connectionMock.object);
+            connectionMock.expects("query").once().withArgs("CALL user_create('" + userName + "','" + password + "')").callsArgWith(1, null, connectionMock.object);
+            connectionMock.expects("escape").twice().returnsArg(0);
 
-            jumpstartUserManager.createUser('pritesh.shah@magnet.com', 'Magnet435', function(){});
+            var called = false;
+            var callback = function() {
+                called = true;
+            };
+
+            JumpStartUserManager.createUser(userName, password, callback);
+
+            expect(called).toBeTruthy();
 
             // http://stackoverflow.com/questions/4144686/how-to-write-a-test-which-expects-an-error-to-be-thrown
             // Anonymous function is needed
@@ -51,9 +81,17 @@ describe("JumpStartUserManager", function() {
 
     describe("updateUser", function() {
         it("should succeed if the underlying stored procedure succeeds", function() {
-            connectionMock.expects("query").once().withArgs("CALL user_update('pritesh.shah@magnet.com','Magnet435')").callsArgWith(1, null, connectionMock.object);
+            connectionMock.expects("query").once().withArgs("CALL user_update('" + userName + "','" + password + "')").callsArgWith(1, null, connectionMock.object);
+            connectionMock.expects("escape").twice().returnsArg(0);
 
-            jumpstartUserManager.updateUser('pritesh.shah@magnet.com', 'Magnet435', function(){});
+            var called = false;
+            var callback = function() {
+                called = true;
+            };
+
+            JumpStartUserManager.updateUser(userName, password, callback);
+
+            expect(called).toBeTruthy();
 
             expect(function(){ poolMock.verify() }).not.toThrow();
             expect(function(){ connectionMock.verify() }).not.toThrow();
@@ -62,9 +100,17 @@ describe("JumpStartUserManager", function() {
 
     describe("deleteUser", function() {
         it("should succeed if the underlying stored procedure succeeds", function() {
-            connectionMock.expects("query").once().withArgs("CALL user_delete('pritesh.shah@magnet.com')").callsArgWith(1, null, connectionMock.object);
+            connectionMock.expects("query").once().withArgs("CALL user_delete('" + userName + "')").callsArgWith(1, null, connectionMock.object);
+            connectionMock.expects("escape").once().returnsArg(0);
 
-            jumpstartUserManager.deleteUser('pritesh.shah@magnet.com', function(){});
+            var called = false;
+            var callback = function() {
+                called = true;
+            };
+
+            JumpStartUserManager.deleteUser(userName, callback);
+
+            expect(called).toBeTruthy();
 
             expect(function(){ poolMock.verify() }).not.toThrow();
             expect(function(){ connectionMock.verify() }).not.toThrow();
