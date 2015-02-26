db2bbefb1b30328f99480ba73db496317516fc06
unknown <skwok@testbox01-w7.kryptonitesys.com>
2015-02-25 16:40:39 -0800

    MOB-1804. Added mmx-console.bat in parity to the .sh
    Former-commit-id: a68f0292aa369f22c5a3de36a6ff82c3501cb3eb
    Former-commit-id: 4e6f3831c6eef00bd8d0164e30dc79363a2f6186
    Former-commit-id: fc3eee49aeb244460172df7dd29a33e6ff3bc51a
    Former-commit-id: 5db7d37d4a0e25346adb35e300c0eb1b9bfd5ca5

---
+++ mmx-console.bat
@@ -0,0 +1,60 @@
+@echo off
+
+setlocal
+
+if "%selfWrapped%"=="" (
+  SET selfWrapped=true
+  %ComSpec% /s /c ""%~0" %*"
+  GOTO :EOF
+)
+
+set TITLE="MagnetMessagingConsole"
+set PROG="mmx-console"
+
+
+
+if "start"=="%1" (
+	call :start
+) else (
+	if "stop"=="%1" (
+		call :stop
+	) else (
+		if "restart"=="%1" (
+			call :restart
+		) else (
+			echo Usage: %0 {start^|stop^|restart}
+			exit /b
+		)
+	)
+) 
+endlocal
+goto :end
+
+
+
+:start
+	if exist %PROG%.pid (
+		echo Error! %PROG% is already running or you have a stale pid file. If %PROG% is not running delete %PROG%.pid file and restart"
+		exit /b 1
+	)	
+	start %TITLE% node start.js	
+	FOR /F "usebackq tokens=2" %%i IN (`tasklist /nh /fi "WINDOWTITLE eq %TITLE%"`) DO echo %%i > .\%PROG%.pid
+goto :end
+
+
+
+:stop
+	del .\%PROG%.pid
+	taskkill /f /im node.exe /t
+goto :end
+
+
+
+:restart
+	call :stop
+	call :start
+goto :end
+
+
+
+:end
