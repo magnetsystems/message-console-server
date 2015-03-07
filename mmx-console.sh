c4a51efca9fe8b7c25de07be4b71313ef7023de3
skwok <stephen.kwok@magnet.com>
2015-03-06 17:28:51 -0800

    MOB-1889: finished updating mmx-console.sh
    Former-commit-id: 0857085398592a3deb54efe3dec82d5442031a0a
    Former-commit-id: 397dc85fa854b983584b097c0d3b6b9a05dfb8c2
    Former-commit-id: 8aa8c2340a280c15bcd3f0c64bb613e7c6d3bb5b
    Former-commit-id: 8ecbddb1ebf0e5e1469a076d7fff5a90922b9ec6

--- mmx-console.sh
+++ mmx-console.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
+PORT_TO_CHECK=3000
 
 PROG="mmx-console"
 PID_PATH="./"
 pid=
+
+check_ports() {
+    if lsof -Pi :$PORT_TO_CHECK -sTCP:LISTEN -t >/dev/null ; then
+        echo "TCP port $PORT_TO_CHECK is already in use, cannot start console"
+        exit 1
+    else
+        echo
+        echo "Using ports $PORT_TO_CHECK"
+        echo
+    fi
+}
+
+check_cmd() {
+    type "$1" &> /dev/null;
+}
+
+check_node() {
+    if check_cmd node || check_cmd nodejs || [ -e node*/bin/node ] ; then
+        echo "node.js is installed"
+    else
+        echo "node.js is not installed"
+        echo "MMX console needs node.js version 10.0 OR higher"
+        echo "Please check http://nodejs.org/download/"
+        exit 1
+    fi
+}
+
 
 start() {
 	if [ -e "$PID_PATH/$PROG.pid" ]; then
@@ -10,6 +38,8 @@ start() {
 		echo "Error! $PROG is already running or you have a stale pid file. If $PROG is not running delete $PID_PATH/$PROG.pid file and restart" 1>&2
 		exit 1
 	else
+            check_ports
+            check_node
 	    	if type -p nodejs >/dev/null; then
                 nohup nodejs start.js > mmx-console.out 2>&1&
             else
