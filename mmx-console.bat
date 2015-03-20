2bb87ed8b002ebd9acd41c6acad18b40466ddfa9
unknown <skwok@testbox01-w7.kryptonitesys.com>
2015-03-19 18:07:15 -0700

    Recheckin mmx-console.bat
    Former-commit-id: 28b775ce2dbaf240d098fd6eee40bd7d9fecd34a
    Former-commit-id: eec74d7e94db8ea6b6f0f8ffd6dca187e21b5cf5
    Former-commit-id: a34a69a53e3657d5828abd8fe0473be1fa03dc98
    Former-commit-id: 0aab4e319fdde76666c853b6ee188b41500cf100

--- mmx-console.bat
+++ mmx-console.bat
@@ -1,8 +1,13 @@
 @echo off
 
-
-
 setlocal
+rem Change the following if it has been changed to use another port
+set PORT_TO_CHECK=3000
+
+set check_port=true
+set TITLE="MagnetMessagingConsole"
+set PROG="mmx-console"
+
 
 if "%selfWrapped%"=="" (
   SET selfWrapped=true
@@ -10,10 +15,10 @@ if "%selfWrapped%"=="" (
   GOTO :EOF
 )
 
-set TITLE="MagnetMessagingConsole"
-set PROG="mmx-console"
-
-
+if "-p"=="%1" (
+	set check_port=false
+	shift
+)	
 
 if "start"=="%1" (
 	call :start
@@ -24,8 +29,7 @@ if "start"=="%1" (
 		if "restart"=="%1" (
 			call :restart
 		) else (
-			echo Usage: %0 {start^|stop^|restart}
-			exit /b
+			call :print_usage
 		)
 	)
 ) 
@@ -34,10 +38,27 @@ goto :end
 
 
 
+:print_usage
+	echo Usage: mmx-console.bat [-p] {start^|stop^|restart}
+	echo.
+	echo Start, stop, or restart the Magnet Mesaging console.
+	echo. 
+	echo Options:
+	echo    -p    No port check when starting.
+	echo. 
+exit /b
+
+
+
 :start
+	if %check_port%==true (
+		call :check_ports
+	)
+	call :check_node
+
 	if exist %PROG%.pid (
-		echo Error! %PROG% is already running or you have a stale pid file. If %PROG% is not running delete %PROG%.pid file and restart"
-		exit /b 1
+		echo Error! %PROG% is already running or you have a stale pid file. If %PROG% is not running delete %PROG%.pid file and try again.
+		exit 1
 	)	
 	start %TITLE% node start.js	
 	FOR /F "usebackq tokens=2" %%i IN (`tasklist /nh /fi "WINDOWTITLE eq %TITLE%"`) DO echo %%i > .\%PROG%.pid
@@ -45,9 +66,39 @@ goto :end
 
 
 
+:check_ports
+	netstat -aon | findstr "%PORT_TO_CHECK%" 1>NUL
+	if %ERRORLEVEL% equ 0 (
+		echo TCP port "%PORT_TO_CHECK%" is already in use, cannot start messaging server
+		exit 1
+	)
+	echo Using ports "%PORT_TO_CHECK%"
+goto :eof
+
+
+
+:check_node
+	node -v 1>NUL 2>NUL
+	if 0 neq %ERRORLEVEL% (
+		if not exist "nodejs\node.exe" (
+			echo node.js is not installed
+			echo MMX console needs node.js version 10.0 OR higher
+			echo Please check https://nodejs.org/download/
+			exit 1
+		) else (
+			echo node.js is installed
+		)
+	) else (
+			echo node.js is installed
+	)
+goto :eof
+
+
+
 :stop
+	set /p pid=<.\%PROG%.pid
+	taskkill /f /pid %pid% /t
 	del .\%PROG%.pid
-	taskkill /f /im node.exe /t
 goto :end
 
 
