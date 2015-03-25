17563b29d269cf8601bbed3fd3bf393d1be58131
unknown <skwok@testbox01-w7.kryptonitesys.com>
2015-03-25 15:11:31 -0700

    MOB-1992: mmx windows installation. Updated mmx-console.bat to use the bundled nodejs if it is available
    Former-commit-id: fcb8a2b9b29fa38cb7da909786ac842008d7650d
    Former-commit-id: cba4ab106cc3f92b1ffd1696f0f6f3f264256aa4
    Former-commit-id: f86792040371dbec1fa800c06a138847f9e60805
    Former-commit-id: fe56ed3394f635441edc68e86b5b042eb35d910f

--- mmx-console.bat
+++ mmx-console.bat
@@ -7,6 +7,7 @@ set PORT_TO_CHECK=3000
 set check_port=true
 set TITLE="MagnetMessagingConsole"
 set PROG="mmx-console"
+set whichnode=bundle
 
 
 if "%selfWrapped%"=="" (
@@ -60,7 +61,12 @@ exit /b
 		echo Error! %PROG% is already running or you have a stale pid file. If %PROG% is not running delete %PROG%.pid file and try again.
 		exit 1
 	)	
-	start %TITLE% node start.js	
+	if bundle==%whichnode% (
+		start %TITLE% .\node start.js
+	) else ( 
+		start %TITLE% node start.js
+	)
+	timeout /t 3
 	FOR /F "usebackq tokens=2" %%i IN (`tasklist /nh /fi "WINDOWTITLE eq %TITLE%"`) DO echo %%i > .\%PROG%.pid
 goto :end
 
@@ -78,18 +84,28 @@ goto :eof
 
 
 :check_node
+	REM check in path
 	node -v 1>NUL 2>NUL
-	if 0 neq %ERRORLEVEL% (
-		if not exist "nodejs\node.exe" (
+	set nodeinpath=%ERRORLEVEL%
+
+	REM for the bundled node
+	.\node -v 1>NUL 2>NUL
+	set nodebundle=%ERRORLEVEL%
+
+	
+	if 0 neq %nodebundle% (
+		if 0 neq %nodeinpath% (
 			echo node.js is not installed
-			echo MMX console needs node.js version 10.0 OR higher
+			echo MMX console needs node.js version 0.10.0 OR higher
 			echo Please check https://nodejs.org/download/
 			exit 1
 		) else (
 			echo node.js is installed
+			set whichnode=env
 		)
 	) else (
-			echo node.js is installed
+		echo node.js is available
+		set whichnode=bundle
 	)
 goto :eof
 
