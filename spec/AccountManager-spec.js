04a350c33a7c362dd47528bbb22c2601b5aeb28b
Edward Yang <edward.yang@magnet.com>
2015-04-03 15:13:23 -0700

    more test fix
    Former-commit-id: 236ab3f633f1b8bd4ff96e13d9ac9cc6b47d34f1
    Former-commit-id: 6b24e4458755e1278f7217242e161c42b7dc6cc5
    Former-commit-id: 8aa4567f7aec29584064be226160cda638e5ca8d
    Former-commit-id: c2b13417c93d1b05633782be64f08ca195e88b83

--- spec/AccountManager-spec.js
+++ spec/AccountManager-spec.js
@@ -56,15 +56,21 @@ describe('AccountManager', function(){
         password  : password
     };
 
-    it('should set up test users', function(done){
-        orm.setup('./lib/models', function(){
-            orm.model('User').create(user1).then(function(res1){
-                expect(res1.lastName).toEqual(user1.lastName);
-                orm.model('User').create(user2).then(function(res2){
-                    expect(res2.lastName).toEqual(user2.lastName);
-                    orm.model('User').create(user3).then(function(res3){
-                        expect(res3.lastName).toEqual(user3.lastName);
-                        done();
+    describe('setup', function(){
+
+        it('should set up test users', function(done){
+            orm.setup('./lib/models', function(){
+                orm.model('User').create(user1).then(function(res1){
+                    expect(res1.lastName).toEqual(user1.lastName);
+                    orm.model('User').create(user2).then(function(res2){
+                        expect(res2.lastName).toEqual(user2.lastName);
+                        orm.model('User').create(user3).then(function(res3){
+                            expect(res3.lastName).toEqual(user3.lastName);
+                            done();
+                        }).catch(function(e){
+                            expect(e).toEqual('failed-test');
+                            done();
+                        });
                     }).catch(function(e){
                         expect(e).toEqual('failed-test');
                         done();
@@ -73,57 +79,47 @@ describe('AccountManager', function(){
                     expect(e).toEqual('failed-test');
                     done();
                 });
-            }).catch(function(e){
-                expect(e).toEqual('failed-test');
-                done();
             });
         });
+
     });
 
     describe('login', function(){
 
-        describe('should fail', function(){
-
-            it("if input is invalid", function(done) {
-                AccountManager.manualLogin(3233, 'test', function(e, user){
-                    expect(e).toEqual('invalid-login');
-                    done();
-                });
+        it("should fail if input is invalid", function(done) {
+            AccountManager.manualLogin(3233, 'test', function(e, user){
+                expect(e).toEqual('invalid-login');
+                done();
             });
-
-            it("if the email didn't exist", function(done) {
-                AccountManager.manualLogin('non-exist@magnet.com', 'test', function(e, user){
-                    expect(e).toEqual('invalid-login');
-                    done();
-                });
-            });
-
-            it("if the user is not approved", function(done) {
-                AccountManager.manualLogin(user2.email, 'admin', function(e, u){
-                    expect(e).toEqual('account-locked');
-                    done();
-                });
-            });
-
-            it("if the password didn't match", function(done) {
-                AccountManager.manualLogin(user3, 'invalid-pass', function(e, u){
-                    expect(e).toEqual('invalid-login');
-                    done();
-                });
-            });
-
         });
 
-        describe('should succeed', function(){
-
-            it("if the credentials are valid", function(done) {
-                AccountManager.manualLogin(user1.email, 'admin', function(e, user){
-                    expect(e).toBeNull();
-                    expect(user.email).toEqual(user1.email);
-                    done();
-                });
+        it("should fail if the email didn't exist", function(done) {
+            AccountManager.manualLogin('non-exist@magnet.com', 'test', function(e, user){
+                expect(e).toEqual('invalid-login');
+                done();
             });
+        });
 
+        it("should fail if the user is not approved", function(done) {
+            AccountManager.manualLogin(user2.email, 'admin', function(e, u){
+                expect(e).toEqual('account-locked');
+                done();
+            });
+        });
+
+        it("should fail if the password didn't match", function(done) {
+            AccountManager.manualLogin(user3, 'invalid-pass', function(e, u){
+                expect(e).toEqual('invalid-login');
+                done();
+            });
+        });
+
+        it("should succeed if the credentials are valid", function(done) {
+            AccountManager.manualLogin(user1.email, 'admin', function(e, user){
+                expect(e).toBeNull();
+                expect(user.email).toEqual(user1.email);
+                done();
+            });
         });
 
     });
