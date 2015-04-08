4e46d984616fde5f0b32a6cf4435e4880ac37aea
unknown <skwok@testbox01-w7.kryptonitesys.com>
2015-04-08 14:26:37 -0700

    Updated Windows bat for MOB-2209
    Former-commit-id: 62757dc3bb2b1259928ade7aedb99d6d1ab15915
    Former-commit-id: 45c69c149d4a1fb6261425050cbe29b4a809e518
    Former-commit-id: 257240570699c166713d6586eaac772d6f3987f3
    Former-commit-id: 9183aac54e7ab23642fc32a6f711a9e299679353

--- mmx-console.bat
+++ mmx-console.bat
@@ -1,20 +1,27 @@
 @echo off
 
+rem Please change .\startup.properties if you would like to use ports other than the default ones.
+rem For detail, please reference the troubleshooting guide.
+
 setlocal
-rem Change the following if it has been changed to use another port
-set PORT_TO_CHECK=3000
-
-set check_port=true
-set TITLE="MagnetMessageConsole"
-set PROG="mmx-console"
-set whichnode=bundle
-
 
 if "%selfWrapped%"=="" (
   SET selfWrapped=true
   %ComSpec% /s /c ""%~0" %*"
   GOTO :EOF
 )
+
+set check_port=true
+set TITLE="Administrationwebinterface"
+set PROGNAME="Administration web interface"
+set PROG="mmx-console"
+set whichnode=bundle
+
+
+
+call :check2Args %*
+
+call :loadPorts ..\startup.properties
 
 if "-p"=="%1" (
 	set check_port=false
@@ -42,7 +49,7 @@ goto :end
 :print_usage
 	echo Usage: mmx-console.bat [-p] {start^|stop^|restart}
 	echo.
-	echo Start, stop, or restart the Magnet Message console.
+	echo Start, stop, or restart the %PROGNAME%.
 	echo. 
 	echo Options:
 	echo    -p    No port check when starting.
@@ -53,35 +60,83 @@ exit /b
 
 :start
 	if %check_port%==true (
-		call :check_ports
+		call :check_port_list %consolePort%
 		echo. 
 	)
+
 	call :check_node
 
 	if exist %PROG%.pid (
 		echo.
-		echo Error! Magnet Message console is already running or you have a stale pid file. If Magnet Message console is not running, then please delete mmx-standalone-dist-win\console\mmx-console.pid file and try again.
+		echo Error! %PROGNAME% is already running or you have a stale pid file. If %PROGNAME% is not running, then please delete mmx-standalone-dist-win\console\mmx-console.pid file and try again.
 		exit 1
 	)	
+
 	if bundle==%whichnode% (
 		start %TITLE% .\node start.js
 	) else ( 
 		start %TITLE% node start.js
 	)
+
 	timeout /t 3 >nul
+
 	FOR /F "usebackq tokens=2" %%i IN (`tasklist /nh /fi "WINDOWTITLE eq %TITLE%"`) DO echo %%i > .\%PROG%.pid
+	set /p pid=<.\%PROG%.pid
+	if "No "=="%pid%" (
+		if bundle==%whichnode% (
+			echo Error starting the web interface. It could be an environment issue. Please execute .\node start.js and fix the environment accordingly.
+		) else ( 
+			echo Error starting the web interface. It could be an environment issue. Please execute node start.js and fix the environment accordingly.
+		)
+		del .\%PROG%.pid
+		exit 1
+	)
 goto :end
 
 
 
-:check_ports
-	netstat -aon | findstr "%PORT_TO_CHECK%" 1>NUL
-	if %ERRORLEVEL% equ 0 (
-		echo.
-		echo Error! TCP port "%PORT_TO_CHECK%" is already in use; thus, cannot start Magnet Message. Please refer to readme.htm on how to change the ports.
-		exit 1
+:check2Args
+	setlocal EnableDelayedExpansion
+	FOR %%A in (%*) DO (
+	        SET /A args_count+=1
+	        if !args_count! equ 3 (
+	                call :print_usage
+	                exit
+	        )
 	)
-	echo Using ports "%PORT_TO_CHECK%"
+
+	if !args_count! equ 0 (
+	        call :print_usage
+	        exit
+	)
+	endlocal
+goto :eof
+
+
+
+:loadPorts
+	FOR /F "eol=; tokens=2 delims==" %%i IN ('findstr /i "consolePort=" %1') do SET consolePort=%%i
+	FOR /F "eol=; tokens=2 delims==" %%i IN ('findstr /i "httpPort=" %1') do SET httpPort=%%i
+goto :eof
+
+
+
+:check_port
+        netstat -aon | findstr "%1" 1>NUL
+        if %ERRORLEVEL% equ 0 (
+                echo.
+                echo Error! TCP port "%1" is already in use; thus, cannot start %PROGNAME%. Please refer to readme.htm on how to change the ports.
+                exit 1
+        )
+        echo Validated port "%1"
+goto :eof
+
+
+
+:check_port_list
+	for %%a in (%*) do (
+        	call :check_port %%a
+	)
 goto :eof
 
 
@@ -119,7 +174,7 @@ goto :eof
 		taskkill /f /pid !pid! /t >nul
 		del .\%PROG%.pid
 	) else (
-		echo Magnet Message console is not running
+		echo %PROGNAME% is not running
 	)
 	endlocal
 goto :end
